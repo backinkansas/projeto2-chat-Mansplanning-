2be5e7a6856173d11e64ca6c1cfea71b43755692
backinkansas <milena.martinsc@gmail.com>
2018-04-11 09:51:49 -0300

    As imagens já mudam. O botão jogar novamente funciona. Falta guardar os clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,49 @@
-let acertos = 0;
-let bancoDeImagens = ['./assets/img/02-sim.png', './assets/img/03-sim.png', './assets/img/04-sim.png', './assets/img/05-nao.png', './assets/img/06-nao.png', './assets/img/07-sim.png', './assets/img/08-sim.png', './assets/img/09-nao.png', './assets/img/10-sim.png']
-let i;
-let btnsim = document.getElementById('btnsim');
+let bancoDeImagens = [
+    '01-sim.png',
+    '02-sim.png',
+    '03-sim.png',
+    '04-sim.png',
+    '05-nao.png',
+    '06-nao.png',
+    '07-sim.png',
+    '08-sim.png',
+    '09-nao.png',
+    '10-sim.png'
+];
+
+let indiceBancoImgs = 1;
+// let valor = document.getElementById("btnsim").getAttribute("value");
+// console.log(valor);
 
 
-let mudarPergunta = function () {
-    for (i in bancoDeImagens) {
-        document.getElementById('imagem').src = bancoDeImagens[i];
+function mudarPergunta() {
+    if (indiceBancoImgs < 9) {
+        indiceBancoImgs++;
+        document.getElementById('imagem').src = "./assets/img/" + bancoDeImagens[indiceBancoImgs];
+    } else {
+    window.location.replace("resultado.html");
     }
-};
+}
+
+// document.querySelectorAll('.botao').onclick = mudarPergunta();
+
+function jogarNovamente () {
+    window.location.replace("index.html");
+}
+
+const gabarito = [
+    "sim",
+    "sim",
+    "sim",
+    "sim",
+    "nao",
+    "nao",
+    "sim",
+    "sim",
+    "nao",
+    "sim"
+];
+
+let respostas = [];
+
+
